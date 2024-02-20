0c88fe18a55008a84e0f008f8cd8f8c45080d23f
d.ushakov <d.ushakov@steel.mil.ua>
2024-02-20 16:30:52 +0200

    Home work   lesson_01

---
+++ lessons_1/homework.js
@@ -0,0 +1,43 @@
+// Ріень 1
+
+/* const age = "38";
+const userName = "Danylo";
+
+
+console.log(age);
+console.log(userName);
+ */
+
+/* const age = "38";
+const userName = "Danylo Ushakov";
+
+console.log(age);
+console.log(userName); */
+
+
+// Ріень 2
+
+/* alert ("Мене звати " + userName);
+alert ("мені " + age + " років");
+
+// альтернатива в один рядок
+alert ("Мене звати " + userName + " мені " + age + " років") */
+
+
+/* const age = "38";
+const userName = "Danylo";
+
+let = prompt("мене звати " + userName + " мені " + age + " років"); */
+
+let number1 = +prompt ("Nubber1");
+let number2 = +prompt ("Nubber2");
+
+let sum = number1 + number2;
+let sub = number1 - number2;
+let mul = number1 * number2;
+let div = number1 / number2;
+
+console.log (sum);
+console.log (sub);
+console.log (mul);
+console.log (div);
---
+++ lessons_1/main.js
@@ -0,0 +1,27 @@
+/* let a1 = 2;
+let b = 3;
+
+let sum = a1 + b;
+a1 = 2;  
+let mul = a1 * b;
+
+const  superHero = "Peter Parker";
+// kkjdflghkldfghfldsfg
+console.log(sum);
+console.log(mul); */
+
+/* const userName = prompt("Як вас звуть,");
+const product = prompt("Введіть назву товару");
+const postOfficeNumber = prompt("Номер відділеня нової пошти");
+
+/* console.log
+(userName + ", ви придбали у нашому магазині " + product + 
+" Товар буде доставлено на відділення N " + postOfficeNumber); */
+
+/* console.log(
+`${userName}  ви придбали у нашому магазині ${product} Товар буде доставлено на відділення N ${postOfficeNumber}.`
+); */ 
+
+let num1 = +prompt("Введіть перше число:");
+let num2 = +prompt("Введіть друге число:");
+console.log(num1 + num2);
